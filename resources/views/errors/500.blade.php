3f279fcc0b814bcf7977aeeb565b27d7eeaeeb96
mtvbrianking@gmail.com <mtvbrianking@gmail.com>
2019-01-22 17:39:45 +0300

    Error views

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,12 @@
+@extends('errors.illustrated-layout')
+
+@section('code', '500')
+
+@section('title', 'Error')
+
+@section('image')
+<div style="background-image: url('svg/500.svg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+</div>
+@endsection
+
+@section('message', 'Whoops, something went wrong on our servers.' )
